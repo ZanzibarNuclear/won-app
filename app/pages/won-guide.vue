8e73832624c3d5eded9203848a82e38b7be818e0
Zanzibar <zanzisworld42@gmail.com>
2025-03-23 17:01:10 -0700

    Limit size of Zanzi's head

--- app/pages/won-guide.vue
+++ app/pages/won-guide.vue
@@ -8,7 +8,7 @@
     >
       <template #leading>
         <UTooltip text="Zanzibar, Nuclear Hero">
-          <NuxtImg src="/images/Zanzibar.jpg" sizes="120px" />
+          <NuxtImg src="/images/Zanzibar.jpg" sizes="250px" />
         </UTooltip>
       </template>
     </UPageSection>
@@ -19,48 +19,45 @@
 const content = reactive({
   title: "Zanzibar's World of Nuclear Energy",
   description:
-    "Welcome! Our mission is bring the world up to speed on the incredible goodness of nuclear energy. We are glad you found us.",
-  icon: "i-ph-globe-duotone",
-});
+    'Welcome! Our mission is bring the world up to speed on the incredible goodness of nuclear energy. We are glad you found us.',
+  icon: 'i-ph-globe-duotone',
+})
 
 const features = computed(() => {
   return [
     {
-      title: "Adventure",
-      description:
-        "Try this role-playing game where the fate of the world is in your hands.",
-      icon: "i-ph-star-duotone",
-      to: "/nu-adventure",
+      title: 'Adventure',
+      description: 'Try this role-playing game where the fate of the world is in your hands.',
+      icon: 'i-ph-star-duotone',
+      to: '/nu-adventure',
     },
     {
-      title: "Flux",
-      description:
-        "Join the conversation in our social app for nuclear enthusiasts.",
-      icon: "i-ph-lightning-duotone",
-      to: "/flux-app",
+      title: 'Flux',
+      description: 'Join the conversation in our social app for nuclear enthusiasts.',
+      icon: 'i-ph-lightning-duotone',
+      to: '/flux-app',
     },
     {
-      title: "Lessons",
-      description: "Learn about nuclear energy and other brainy topics.",
-      icon: "i-ph-student-duotone",
-      to: "/get-smarter",
+      title: 'Lessons',
+      description: 'Learn about nuclear energy and other brainy topics.',
+      icon: 'i-ph-student-duotone',
+      to: '/get-smarter',
     },
     {
-      title: "Essays",
+      title: 'Essays',
       description:
-        "Perspectives and news about anything related to the productive uses of nuclear.",
-      icon: "i-ph-newspaper",
-      to: "https://blog.worldofnuclear.com",
+        'Perspectives and news about anything related to the productive uses of nuclear.',
+      icon: 'i-ph-newspaper',
+      to: 'https://blog.worldofnuclear.com',
     },
     {
       title: "Who's Who",
-      description:
-        "Profiles of people who appreciate nuclear energy. Set up and share yours.",
-      icon: "i-ph-newspaper",
-      to: "/profiles-in-nuclear",
+      description: 'Profiles of people who appreciate nuclear energy. Set up and share yours.',
+      icon: 'i-ph-newspaper',
+      to: '/profiles-in-nuclear',
     },
-  ];
-});
+  ]
+})
 </script>
 
 <style lang="scss" scoped></style>
