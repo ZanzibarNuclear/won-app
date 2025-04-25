992407011c1ad9d650a3430b51a18674c04a8e5b
Zanzibar <zanzisworld42@gmail.com>
2025-04-25 08:07:18 -0700

    center member profile

--- app/pages/profiles-in-nuclear/[handle]/index.vue
+++ app/pages/profiles-in-nuclear/[handle]/index.vue
@@ -1,65 +1,67 @@
 <template>
-  <UPage>
-    <UPageHeader
-      v-if="profile && !notFound"
-      class="m-6"
-      headline="Member Profile"
-      :title="profile.alias || 'Genius'"
-      :description="
-        profile.whyNuclear ||
-        profile.whyJoined ||
-        'Exploring nuclear energy and its many productive uses.'
-      "
-    />
-    <UPageBody class="m-6 w-4/5">
-      <div v-if="profile && !notFound">
-        <div class="my-8">
-          <MemberProfilePicture :src="profile.glamShot ?? undefined" class="w-[400px]" />
-          <MemberNameTag
-            class="my-6"
-            :alias="profile.alias ?? undefined"
-            :handle="profile.handle ?? undefined"
-            :avatar-src="profile.avatar ?? undefined"
-          />
-        </div>
-        <div class="space-y-8">
-          <div>
-            <div class="facet-label">About {{ profile.alias }}:</div>
-            <div>{{ profile.bio }}</div>
+  <UContainer class="flex flex-col items-center w-4/5">
+    <UPage>
+      <UPageHeader
+        v-if="profile && !notFound"
+        class="m-6"
+        headline="Member Profile"
+        :title="profile.alias || 'Genius'"
+        :description="
+          profile.whyNuclear ||
+          profile.whyJoined ||
+          'Exploring nuclear energy and its many productive uses.'
+        "
+      />
+      <UPageBody>
+        <div v-if="profile && !notFound">
+          <div class="my-8">
+            <MemberProfilePicture :src="profile.glamShot ?? undefined" class="w-[400px]" />
+            <MemberNameTag
+              class="my-6"
+              :alias="profile.alias ?? undefined"
+              :handle="profile.handle ?? undefined"
+              :avatar-src="profile.avatar ?? undefined"
+            />
           </div>
-          <div class="italic">
-            {{ profile.alias }} has been a member of the World of Nuclear since
-            {{ formatDate(profile.createdAt) }}.
-          </div>
-          <div class="flex flex-row gap-x-12">
+          <div class="space-y-8">
             <div>
-              <div class="facet-label">Location:</div>
-              <div>{{ profile.location }}</div>
-              <div v-if="profile.website">
-                Also at <NuxtLink :to="profile.website" target="_blank">this website</NuxtLink>.
+              <div class="facet-label">About {{ profile.alias }}:</div>
+              <div>{{ profile.bio }}</div>
+            </div>
+            <div class="italic">
+              {{ profile.alias }} has been a member of the World of Nuclear since
+              {{ formatDate(profile.createdAt) }}.
+            </div>
+            <div class="flex flex-row gap-x-12">
+              <div>
+                <div class="facet-label">Location:</div>
+                <div>{{ profile.location }}</div>
+                <div v-if="profile.website">
+                  Also at <NuxtLink :to="profile.website" target="_blank">this website</NuxtLink>.
+                </div>
+              </div>
+              <div>
+                <div class="facet-label">Karma points:</div>
+                <div>{{ profile.karmaScore }}</div>
               </div>
             </div>
             <div>
-              <div class="facet-label">Karma points:</div>
-              <div>{{ profile.karmaScore }}</div>
+              <div class="facet-label">Motivation for joining the World of Nuclear:</div>
+              <div>{{ profile.whyJoined || 'no comment' }}</div>
+            </div>
+            <div>
+              <div class="facet-label">Why support nuclear energy?:</div>
+              <div>{{ profile.whyNuclear || 'no comment' }}</div>
             </div>
           </div>
-          <div>
-            <div class="facet-label">Motivation for joining the World of Nuclear:</div>
-            <div>{{ profile.whyJoined || 'no comment' }}</div>
-          </div>
-          <div>
-            <div class="facet-label">Why support nuclear energy?:</div>
-            <div>{{ profile.whyNuclear || 'no comment' }}</div>
-          </div>
         </div>
-      </div>
-      <div v-else>
-        <h1>Not Found</h1>
-        <div>Sorry, we did not find the profile you are looking for.</div>
-      </div>
-    </UPageBody>
-  </UPage>
+        <div v-else>
+          <h1>Not Found</h1>
+          <div>Sorry, we did not find the profile you are looking for.</div>
+        </div>
+      </UPageBody>
+    </UPage>
+  </UContainer>
 </template>
 
 <script setup lang="ts">
