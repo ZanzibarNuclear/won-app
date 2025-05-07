b7fa1aff7e8e31efb3d41c4fc6cd697d3217e1f8
Zanzibar <zanzisworld42@gmail.com>
2025-05-07 12:35:35 -0700

    Move "about blog" aside in favor of blog index. Add / update blog content.

---
+++ app/pages/news-and-views/index-old.vue
@@ -0,0 +1,40 @@
+<template>
+  <UContainer>
+    <UPage title="News and Views">
+      <UPageHero
+        title="News and Views"
+        description="Find out what's going on with nuclear energy. Consider perspectives on the amazing future that nuclear unlocks."
+        :links="links"
+      >
+        <ImagePlaceholder />
+      </UPageHero>
+    </UPage>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+const links = ref([
+  {
+    label: 'Headlines',
+    to: '/news-and-views/blog-headlines',
+    icon: 'i-ph-test-tube-duotone',
+  },
+  {
+    label: 'Jump right in',
+    to: '/news-and-views/what-about-the-waste',
+    icon: 'i-ph-note-pencil-duotone',
+  },
+])
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: var(--color-blue-400);
+  text-decoration: underline;
+}
+a:hover {
+  color: navy;
+  background-color: lightyellow;
+  text-decoration: underline;
+}
+</style>
--- app/pages/news-and-views/index.vue
+++ app/pages/news-and-views/index.vue
@@ -1,40 +1,61 @@
-<template>
-  <UContainer>
-    <UPage title="News and Views">
-      <UPageHero
-        title="News and Views"
-        description="Find out what's going on with nuclear energy. Consider perspectives on the amazing future that nuclear unlocks."
-        :links="links"
-      >
-        <ImagePlaceholder />
-      </UPageHero>
-    </UPage>
-  </UContainer>
-</template>
-
 <script setup lang="ts">
-const links = ref([
-  {
-    label: 'Headlines',
-    to: '/news-and-views/blog-headlines',
-    icon: 'i-ph-test-tube-duotone',
-  },
-  {
-    label: 'Jump right in',
-    to: '/news-and-views/what-about-the-waste',
-    icon: 'i-ph-note-pencil-duotone',
-  },
-])
+const route = useRoute()
+
+const { data: page } = await useAsyncData('blog', () => queryCollection('headlines').first())
+const { data: posts } = await useAsyncData(route.path, () =>
+  queryCollection('posts').where('published', '=', true).order('postedAt', 'DESC').all(),
+)
+
+useSeoMeta({
+  title: page.value?.title,
+  ogTitle: page.value?.title,
+  description: page.value?.description,
+  ogDescription: page.value?.description,
+})
+
+const { cdnEndpoint } = useRuntimeConfig().public
+
+const modifyCdnImageSrc = (image: any) => {
+  if (image.src.indexOf('http') === 0) {
+    return image
+  } else {
+    const modified = Object.assign({}, image, { src: `${cdnEndpoint}/images/${image.src}` })
+    return modified
+  }
+}
+
+defineOgImageComponent('Saas')
 </script>
 
-<style lang="scss" scoped>
-a {
-  color: var(--color-blue-400);
-  text-decoration: underline;
-}
-a:hover {
-  color: navy;
-  background-color: lightyellow;
-  text-decoration: underline;
-}
-</style>
+<template>
+  <UContainer>
+    <UPageHeader v-bind="page" class="py-[50px]" />
+
+    <UPageBody>
+      <UBlogPosts>
+        <UBlogPost
+          v-for="(post, index) in posts"
+          :key="index"
+          :to="post.path"
+          :title="post.title"
+          :description="post.description"
+          :image="modifyCdnImageSrc(post.image)"
+          :date="
+            new Date(post.lastUpdate || '2025-01-01').toLocaleDateString('en', {
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric',
+            })
+          "
+          :badge="post.badge"
+          :orientation="index === 0 ? 'horizontal' : 'vertical'"
+          :class="[index === 0 && 'col-span-full']"
+          variant="naked"
+          :ui="{
+            description: 'line-clamp-2',
+          }"
+        />
+      </UBlogPosts>
+    </UPageBody>
+  </UContainer>
+</template>
